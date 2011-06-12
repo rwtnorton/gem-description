59955950e30297dfd4c66fc5d8536233566ea308
Richard W. Norton <rwtnorton@gmail.com>
2011-06-12 00:45:59 -0400

    Fixed false not-found bug; added comments.

--- gem-description.rb
+++ gem-description.rb
@@ -1,23 +1,36 @@
 #!/usr/bin/env ruby
 
+#
+# Displays the description of a remote gem on stdout.
+#
+
 require 'yaml'
-require 'rubygems' # To have Gem::Specification
+require 'rubygems' # To have Gem::Specification for deserialization
 
 raise 'Requires gem argument' unless ARGV.size == 1
 gem_name = ARGV[0]
 
-spec_yaml = open("|gem specification #{gem_name} --remote") do |f|
-  f.read
-end
-if (spec_yaml =~ /\AERROR:\s+Unknown\s+gem\s+/)
-  STDERR.puts spec
+#
+# Make a remote call for the gem specification.
+#
+spec_yaml = `gem specification #{gem_name} --remote`
+if spec_yaml.empty?
+  STDERR.puts "Unable to gather gem specification"
   exit
 end
 
 spec = YAML::load spec_yaml
-p spec
+
+#
+# This should deserialize to a Gem::Specification but if
+# it lacks the '--- !ruby/object:Gem::Specification' line,
+# it could deserialize to a Hash.
+#
 if spec.respond_to? :description
   puts spec.description
 elsif spec.respond_to? :[]
   puts spec['description']
+else
+  puts "Hmm, not sure what to do with this..."
+  p spec
 end
